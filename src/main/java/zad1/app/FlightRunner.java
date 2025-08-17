e00f4fd4da1b80573a9c0792dff9859a936ea4da
Noemi Barszczewska <noemi.pucek@gmail.com>
2025-08-17 23:42:26 +0200

    another one

--- src/main/java/zad1/app/FlightRunner.java
+++ src/main/java/zad1/app/FlightRunner.java
@@ -1,5 +1,6 @@
 package zad1.app;
 
+import zad1.models.LoadElement;
 import zad1.models.Luggage;
 import zad1.models.Passenger;
 import zad1.models.Plane;
@@ -12,6 +13,7 @@ public class FlightRunner {
         Plane airbus = new Plane("A380", "Airbus", 555, 50000);
         Plane lightAircraft = new Plane("Cessna 172", "Cessna", 4, 400);
 
+
         Passenger johny = new Passenger("Jan", "97031309177", 90);
         Passenger ala = new Passenger("Alicja", "61060806266", 65);
         Passenger stewardess = new Passenger("Karolina", "80041508344", 55);
@@ -23,19 +25,39 @@ public class FlightRunner {
         Luggage luggage3 = new Luggage("Rimowa", 35);
         Luggage luggage4 = new Luggage("Wittchen", 20);
 
+       tryToLoad(lightAircraft, johny);
+       tryToLoad(lightAircraft, ala);
+       tryToLoad(lightAircraft, marek);
+       tryToLoad(lightAircraft, jola);
+       tryToLoad(lightAircraft, stewardess);
 
-        lightAircraft.addToPlane(johny);
-        lightAircraft.addToPlane(ala);
-        lightAircraft.addToPlane(marek);
-        lightAircraft.addToPlane(jola);
-        lightAircraft.addToPlane(stewardess);
-        lightAircraft.addToPlane(luggage1);
-        lightAircraft.addToPlane(luggage2);
-        lightAircraft.addToPlane(luggage3);
+       tryToLoad(lightAircraft, luggage1);
+       tryToLoad(lightAircraft, luggage2);
+       tryToLoad(lightAircraft, luggage3);
 
-        boeing.addToPlane(luggage4);
-        boeing.addToPlane(stewardess);
+       tryToLoad(boeing, luggage4);
+       tryToLoad(boeing, stewardess);
 
+//        lightAircraft.addToPlane(johny);
+//        lightAircraft.addToPlane(ala);
+//        lightAircraft.addToPlane(marek);
+//        lightAircraft.addToPlane(jola);
+//        lightAircraft.addToPlane(stewardess);
+//        lightAircraft.addToPlane(luggage1);
+//        lightAircraft.addToPlane(luggage2);
+//        lightAircraft.addToPlane(luggage3);
+//
+//        boeing.addToPlane(luggage4);
+//        boeing.addToPlane(stewardess);
 
+    }
+
+    private static void tryToLoad(Plane plane, LoadElement loadElement) { //zeby nie pisac try catch do kazdej linijki
+        try {
+            plane.addToPlane(loadElement);
+            System.out.println("Dodano na pokład: " + loadElement);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/zad1/models/Plane.java
+++ src/main/java/zad1/models/Plane.java
@@ -2,6 +2,7 @@ package zad1.models;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Plane {
 
@@ -56,17 +57,39 @@ public class Plane {
                 '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Plane plane = (Plane) o;
+        return numberOfSeats == plane.numberOfSeats && Double.compare(capacityInKg, plane.capacityInKg) == 0 && Double.compare(loadElementsWeight, plane.loadElementsWeight) == 0 && passengerCnt == plane.passengerCnt && Objects.equals(name, plane.name) && Objects.equals(producer, plane.producer) && Objects.equals(loadElements, plane.loadElements);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, producer, numberOfSeats, capacityInKg, loadElementsWeight, passengerCnt, loadElements);
+    }
+
+//    public void addToPlane(LoadElement loadElement) {
+//        try {
+//            loadElement.checkIfCanBeLoaded(this);
+//            loadElements.add(loadElement);
+//            loadElementsWeight += loadElement.getWeightInKg();
+//            if (loadElement instanceof Passenger) {
+//                passengerCnt++;
+//            }
+//            System.out.println("Dodano na pokład: " + loadElement);
+//        } catch (RuntimeException e) {
+//            System.out.println(e.getMessage());
+//        }
+//    }
+
     public void addToPlane(LoadElement loadElement) {
-        try {
-            loadElement.checkIfCanBeLoaded(this);
-            loadElements.add(loadElement);
-            loadElementsWeight += loadElement.getWeightInKg();
-            if (loadElement instanceof Passenger) {
-                passengerCnt++;
-            }
-            System.out.println("Dodano na pokład: " + loadElement);
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
+        loadElement.checkIfCanBeLoaded(this);
+        loadElements.add(loadElement);
+        loadElementsWeight += loadElement.getWeightInKg();
+        if (loadElement instanceof Passenger) {
+            passengerCnt++;
         }
     }
 }
